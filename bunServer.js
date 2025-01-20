2027075948d057bf15b72e28206db28b7caeeb44
mildlybrutal <150813801+mildlybrutal@users.noreply.github.com>
2025-01-20 12:27:19 +0530

    Update bunServer.js

--- bunServer.js
+++ bunServer.js
@@ -4,11 +4,11 @@ serve({
     fetch(request) {
         const url = new URL(request.url);
         if (url.pathname === "/") {
-            return new Response("Hello nigga", { status: 200 });
+            return new Response("Hello lol", { status: 200 });
         } else if (url.pathname === "/nigga") {
-            return new Response("Hello nigga, skibidi toile", { status: 200 });
+            return new Response("Hello lol, skibidi lol", { status: 200 });
         } else {
-            return new Response("404 not found niqqa", { status: 404 });
+            return new Response("404 not found lol", { status: 404 });
         }
     },
     port: 3000,
